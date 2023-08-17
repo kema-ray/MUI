298ad60c69a34166f18b5a681b991f3a6f129299
kema-ray <rachelkemuma99@gmail.com>
2023-08-17 12:44:16 +0300

    create a custom theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { Button } from '@mui/base';
+import { Container, Button, } from '@mui/material';
+import { ThemeProvider } from '@mui/system';
 import { useEffect } from 'react';
 import './App.css';
+import theme from './styles/theme';
 
 function App() {
 
@@ -8,10 +10,17 @@ function App() {
     document.title = "React MUI";
   }, [])
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <Button>Test</Button>
-    </div>
+      <ThemeProvider theme={theme}>
+        <Container
+          maxWidth='xl'
+          sx={{
+            background: "#fff"
+          }}
+        >
+          <h1>Hello World</h1>
+          <Button variant='contained'>Test</Button>
+        </Container>
+      </ThemeProvider>
   );
 }
 
